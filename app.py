0e3b4886e1cfb5e69da43c2c3896ce617e8dd639
amaru-g <gogut@protonmail.com>
2021-12-09 21:37:10 +0100

    external function successful

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from data_handler import adding_values
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "que pedoski"
+    a=6
+    b=6
+    c=adding_values(a,b)
+    return f"que pedoski {c}"
 if __name__=="__main__":
     app.run(debug=True)
---
+++ data_handler.py
@@ -0,0 +1,3 @@
+def adding_values(value_1,value_2):
+    value_3=value_1+value_2
+    return value_3
